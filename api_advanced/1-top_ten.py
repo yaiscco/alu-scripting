1a1eb1ff9df4d2d6947a09ef6f3ab3c2293dcb71
yaiscco <yaimajakacol@gmail.com>
2026-03-22 13:43:27 +0200

    final

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,34 +7,23 @@ import requests
 
 def top_ten(subreddit):
     '''Prints the top ten posts of a subreddit'''
-    if not isinstance(subreddit, str):
-        print(None)
-        return
-
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-
     headers = {
-        "User-Agent": "linux:api_advanced.project:v1.0 (by /u/shobi_ola)"
+        "User-Agent": "MyRedditBot/1.0 by api_advanced"
     }
-
     params = {"limit": 10}
-
     response = requests.get(
         url,
         headers=headers,
         params=params,
         allow_redirects=False
     )
-
     if response.status_code != 200:
         print(None)
         return
-
     data = response.json().get("data", {}).get("children", [])
-
     if not data:
         print(None)
         return
-
     for post in data:
-        print(post.get("data").get("title"))
+        print(post.get("data", {}).get("title"))
